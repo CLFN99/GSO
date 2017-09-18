fc15c3a86b789c6472334d52276523619d8cc1f4
Célina Ngapy <c.ngapy@student.fontys.nl>
2017-09-18 11:35:08 +0200

    addet timetest

---
+++ UnittestenTime/src/fontys/time/TimeTest.java
@@ -0,0 +1,44 @@
+package fontys.time;
+
+class TimeTest {
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getDayInWeek() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getYear() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMonth() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getDay() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getHours() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMinutes() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void plus() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void compareTo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void difference() {
+    }
+
+}
