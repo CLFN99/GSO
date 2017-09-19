387ae06be88e147b58ae766a41bc8f93cdc9c524
MagicLegend <aapje112513@hotmail.nl>
2017-09-19 20:28:04 +0200

    Added TimeSpan unit tests

--- UnittestenTime/src/fontys/tests/TimeSpanTest.java
+++ UnittestenTime/src/fontys/tests/TimeSpanTest.java
@@ -15,42 +15,88 @@ import static org.junit.jupiter.api.Assertions.*;
 class TimeSpanTest {
     @Test
     void testConstructor() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-
+        Throwable exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            TimeSpan ts = new TimeSpan(new Time(2017, 9, 18, 12, 0), new Time(2017, 9, 18, 11, 30));
         });
+        System.out.println(exception.getMessage());
     }
 
 
     @Test
     void getBeginTime() {
+        Time beginTime = new Time(2017, 9, 18, 12, 0);
+        Time endTime = new Time(2017, 9, 18, 12, 30);
+        TimeSpan ts = new TimeSpan(beginTime, endTime);
+        assertEquals(beginTime, ts.getBeginTime());
     }
 
     @Test
     void getEndTime() {
+        Time beginTime = new Time(2017, 9, 18, 12, 0);
+        Time endTime = new Time(2017, 9, 18, 12, 30);
+        TimeSpan ts = new TimeSpan(beginTime, endTime);
+        assertEquals(endTime, ts.getEndTime());
     }
 
     @Test
     void length() {
+        Time beginTime = new Time(2017, 9, 18, 12, 0);
+        Time endTime = new Time(2017, 9, 18, 12, 30);
+        TimeSpan ts = new TimeSpan(beginTime, endTime);
+        assertEquals(endTime.difference(beginTime), ts.length());
     }
 
     @Test
     void setBeginTime() {
+        Throwable exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Time newTime = new Time(2017, 9, 18, 11, 30); //Time 30 mins before the first beginTime
+            TimeSpan ts = new TimeSpan(new Time(2017, 9, 18, 12, 0), new Time(2017, 9, 18, 11, 30));
+            ts.setBeginTime(newTime);
+        });
+        System.out.println(exception.getMessage());
     }
 
     @Test
     void setEndTime() {
+        Throwable exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Time newTime = new Time(2017, 9, 18, 11, 30); //Time 30 mins before the first beginTime
+            TimeSpan ts = new TimeSpan(new Time(2017, 9, 18, 12, 0), new Time(2017, 9, 18, 11, 30));
+            ts.setEndTime(newTime);
+        });
+        System.out.println(exception.getMessage());
     }
 
     @Test
     void move() {
+        //Untestable? The Time.plus function returns a new Time object, so it can't be compared?
+        Time beginTime = new Time(2017, 9, 18, 12, 0);
+        Time endTime = new Time(2017, 9, 18, 12, 30);
+        TimeSpan ts = new TimeSpan(beginTime, endTime);
+        ts.move(30);
+        assertEquals(ts.getBeginTime(), beginTime.plus(30));
+        assertEquals(ts.getEndTime(), endTime.plus(30));
     }
 
     @Test
     void changeLengthWith() {
+        Throwable exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            TimeSpan ts = new TimeSpan(new Time(2017, 9, 18, 12, 0), new Time(2017, 9, 18, 12, 30));
+            ts.changeLengthWith(-10);
+        });
+        System.out.println(exception.getMessage());
+
+        //Untestable? The Time.plus function returns a new Time object, so it can't be compared?
+        Time endTime = new Time(2017, 9, 18, 12, 30);
+        TimeSpan ts = new TimeSpan(new Time(2017, 9, 18, 12, 0), endTime);
+        ts.changeLengthWith(10);
+        assertEquals(ts.getEndTime(), endTime.plus(10));
     }
 
     @Test
     void isPartOf() {
+        TimeSpan ts = new TimeSpan(new Time(2017, 9, 18, 12, 0), new Time(2017, 9, 18, 12, 30));
+        TimeSpan ts2 = new TimeSpan(new Time(2018, 9, 18, 12, 0), new Time(2018, 9, 18, 12, 30));
+        System.out.println(ts.isPartOf(ts2));
     }
 
     @Test
