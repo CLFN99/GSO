adb05fc5e4335af01495b8af791ef320fcd82c84
MagicLegend <aapje112513@hotmail.nl>
2017-10-01 22:11:14 +0200

    Added small test thingie

--- UnittestenTimev2/src/fontys/tests/AppointmentTest.java
+++ UnittestenTimev2/src/fontys/tests/AppointmentTest.java
@@ -1,9 +1,14 @@
 package fontys.tests;
 
 import fontys.agenda.Appointment;
+import fontys.agenda.Contact;
 import fontys.time.Time;
 import fontys.time.TimeSpan;
 import org.junit.jupiter.api.Test;
+
+/**
+ * @author MagicLegend
+ */
 
 public class AppointmentTest {
     @Test
@@ -17,6 +22,12 @@ public class AppointmentTest {
 
     @Test
     public void addContact() {
-
+        Appointment appointment = new Appointment("TestSubject",
+                new TimeSpan(
+                        new Time(2017, 9, 18, 12, 0),
+                        new Time(2017, 9, 18, 12, 30)
+                ));
+        Contact contact = new Contact("Maria Jones");
+        appointment.addContact(contact);
     }
 }
